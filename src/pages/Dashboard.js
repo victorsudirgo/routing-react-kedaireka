1e14616b0a79fe5f8e9545527f1516358f4c0ff1
victorsudirgo <102844982+victorsudirgo@users.noreply.github.com>
2022-10-19 05:40:56 +0700

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import './Dashboard.css'
 import {Link} from 'react-router-dom';
+import { useRef, useState, useEffect } from "react";
 
 function Dashboard() {
+
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const handleChange = (event) => {
+    const selectedFile = event.target.files[0]
+    setFile(selectedFile)
+    const filePreview = URL.createObjectURL(selectedFile)
+    setPreview(filePreview)
+  }
+
+  
+
   return (
     <div className='dashboard'>
-    <h1>Dashboard</h1>
+    <h1></h1>
+
+    {file && <img src={preview} alt={file.name} style={{width: "400px"}}/>}
+    <div className='fileinput'>
+    <input type="file" name="file" onChange={(e) => handleChange(e)}/>
+    </div>
+    
     <Link to='/visualize'>
     <button className='btn-viz' onClick=''>Visualize</button>
     </Link>
